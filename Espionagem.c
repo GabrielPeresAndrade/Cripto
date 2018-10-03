3854b0d7eed19e353a0e73b98cff444d2923dcfc
Gabriel Peres <gabrielperesdeandrade@hotmail.com>
2018-10-02 21:31:59 -0300

    Espionagem terminado

--- Espionagem.c
+++ Espionagem.c
@@ -3,10 +3,11 @@
 #include <string.h>
 
 unsigned int conta_origem(unsigned int texto);
+unsigned int valor_msg(unsigned int texto, unsigned int chave);
 
 int main()
 {
-	unsigned int i,quantidade,texto,cifrada,mensagem;
+	unsigned int i,quantidade,texto,cifrada,mensagem,chave;
 
 	printf("Digite o Texto Claro\n");
 	scanf("%u",&texto);
@@ -15,6 +16,8 @@ int main()
 	printf("Digite o Texto Cifrado\n");
 	scanf("%u",&cifrada);
 	getchar();
+
+	chave = cifrada ^ texto;
 	
 	printf("Digite quantas mensagens cifradas eu tenho\n");
 	scanf("%u",&quantidade);
@@ -27,7 +30,8 @@ int main()
 		getchar();
 		if (conta_origem(cifrada)==conta_origem(mensagem))
 		{
-			printf("Sao da mesma agencia\n");
+			printf("Sao da mesma agencia\nvalor : %u\n",valor_msg(mensagem,chave));
+
 		}
 
 	}
@@ -39,4 +43,9 @@ int main()
 unsigned int conta_origem(unsigned int texto)
 {
 	return (texto >> 24 & 0xff);
+}
+
+unsigned int valor_msg(unsigned int texto,unsigned int chave)
+{
+	return (chave ^ texto) << 16 >> 16;
 }
